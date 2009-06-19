7ab69e2117f0fb3d8af5b0dc4cb2edac48a2eb92
Taesoo Kim <tsgatesv@gmail.com>
2009-06-19 10:26:31 +0900

    initial release

---
+++ pylookup.el
@@ -0,0 +1,252 @@
+(eval-when-compile
+  (require 'browse-url)
+  (require 'simple)
+  (require 'cl))
+
+(require 'ido)
+
+;;=================================================================
+;; user options
+;;=================================================================
+
+(defvar pylookup-db-file "pylookup.db" "Pylookup database file")
+(defvar pylookup-program "pylookup.py" "Pylookup execution file")
+
+;;=================================================================
+;; internal variables
+;;=================================================================
+
+(defvar pylookup-html-locations '("http://docs.python.org"))
+(defvar pylookup-history nil)
+(defvar pylookup-cache nil)
+(defvar pylookup-return-window-config nil)
+(defvar pylookup-temp-buffer-name "*Pylookup Completions*")
+
+(defvar pylookup-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [mouse-1]  'pylookup-mode-lookup-and-leave)
+    (define-key map [mouse-2]  'pylookup-mode-lookup)
+    (define-key map "\C-m"     'pylookup-mode-lookup-and-leave)
+    (define-key map " "        'pylookup-mode-lookup-and-leave)
+    (define-key map "f"        'pylookup-mode-lookup)
+    (define-key map "q"        'pylookup-mode-quit-window)
+    (define-key map "n"        'pylookup-mode-next-line)
+    (define-key map "p"        'pylookup-move-prev-line)
+
+    (define-key map "v"        'scroll-down)
+    (define-key map "V"        'scroll-up)
+    (define-key map "l"        'recenter)
+    (define-key map "<"        'beginning-of-buffer)
+    (define-key map ">"        'end-of-buffer)
+    (define-key map "v"        'scroll-down)
+    
+    map)
+  "Keymap for `pylookup-mode-mode'.")
+
+(put 'pylookup-mode 'mode-class 'special)
+
+;;=================================================================
+;; pylookup mode specific interactive functions
+;;=================================================================
+
+(defun pylookup-trim (desc n)
+  "Trim desc string to fit into the length, n"
+
+  (if (> (length desc) (- n 2))
+      (concat (substring desc 0 (- n 2)) "..")
+    desc))
+
+(defun pylookup-mode ()
+  "Major mode for output from \\[pylookup-lookup]."
+  (interactive)
+  
+  (kill-all-local-variables)
+  (use-local-map pylookup-mode-map)
+  (setq major-mode 'pylookup-mode)
+  (setq mode-name "Pylookup")
+  (setq buffer-read-only t))
+
+(defun pylookup-move-prev-line ()
+  "Move to previous entry"
+  (interactive)
+  
+  (when (< 3 (line-number-at-pos))
+    (call-interactively 'previous-line)))
+
+(defun pylookup-mode-next-line ()
+  "Move to next entry"
+  (interactive)
+  
+  (when (< (line-number-at-pos)
+           (- (line-number-at-pos (point-max)) 1))
+    (call-interactively 'next-line)))
+
+(defun pylookup-mode-lookup-and-leave ()
+  "Lookup the current line in a browser and leave the completions window."
+  (interactive)
+  
+  (call-interactively 'pylookup-mode-lookup)
+  (pylookup-mode-quit-window))
+
+(defun pylookup-mode-lookup ()
+  "Lookup the current line in a browser."
+  (interactive)
+  
+  (let ((url (get-text-property (point) 'pylookup-target-url)))
+    (if url
+        (progn
+          (beginning-of-line)
+          (message "Browsing: \"%s\"" url)
+          (browse-url url))
+      (error "No URL on this line"))))
+
+(defun pylookup-mode-quit-window ()
+  "Leave the completions window."
+  (interactive)
+
+  (set-window-configuration pylookup-return-window-config))
+
+;;=================================================================
+;; execute pylookup
+;;=================================================================
+
+(defun pylookup-exec-get-cache ()
+  "Run a pylookup process and get a list of cache (db key)"
+
+  (split-string
+   (with-output-to-string
+     (call-process pylookup-program nil standard-output nil 
+		   "-d" (expand-file-name pylookup-db-file)
+		   "-c"))))
+
+(defun pylookup-exec-lookup (search-term)
+  "Runs a pylookup process and returns a list of (term, url) pairs."
+
+  (mapcar 
+   (lambda (x) (split-string x ";"))
+   (split-string
+     (with-output-to-string
+         (call-process pylookup-program nil standard-output nil 
+                       "-d" (expand-file-name pylookup-db-file) 
+		       "-l" search-term))
+     "\n" t)))
+
+;;=================================================================
+;; interactive user interfaces
+;;=================================================================
+
+;;;###autoload
+(defun pylookup-lookup (search-term)
+  "Lookup SEARCH-TERM in the Python HTML indexes."
+  (interactive
+   (list 
+    (let ((initial (thing-at-point 'word)))
+      (ido-completing-read  "Lookup Python Documentation for: "
+                            (if pylookup-cache 
+                                pylookup-cache 
+                              (setq pylookup-cache (pylookup-exec-get-cache)))
+                            nil nil initial 'pylookup-history))
+    ))
+
+  (let ((matches (pylookup-exec-lookup search-term)))
+    (cond
+
+      ;; 0. No results.
+      ((eq matches nil)
+       (message "No matches for \"%s\"." search-term))
+
+      ;; 1. A single result.
+      ((= (length matches) 1)  
+       ;; Point the browser at the unique result and get rid of the buffer
+       (let ((data (car matches)))
+         (message "Browsing: \"%s\"" (car data))
+         (browse-url (cadr data))))
+
+      ;; N. Multiple results.
+      (t
+       ;; Decorate the temporary buffer lines with appropriate properties for
+       ;; selection.
+       (let* ((cur-window-conf (current-window-configuration))
+              (tmpbuf (get-buffer-create pylookup-temp-buffer-name))
+              (index 0))
+    
+         (display-buffer tmpbuf)
+         (pop-to-buffer tmpbuf)
+
+         (setq buffer-read-only nil)
+         (erase-buffer)
+
+         ;; Insert the text in the buffer
+         (insert (format "Python index matches for %s:\n\n" search-term))
+         (dolist (x matches)
+            ;; split like
+            ;; waitpid() (in module os) [lib]
+            ;; --------- -------------- -----
+            ;; =>
+            ;; waitpid     (in module os)  [lib]
+            ;; api         module          type
+
+            (let* ((tokens (split-string (car x)))
+                   (api (car tokens))
+                   (iter (cdr tokens))
+                   (type (car (last tokens)))
+                   (module ""))
+
+              (while (not (or (equal iter nil)
+                              (string= (car iter) type)))
+                (setq module (concat module " " (car iter)))
+                (setq iter (cdr iter)))
+
+              (incf index)
+              (insert (format " %03d) %-25s %-30s %10s" 
+			      index 
+			      (pylookup-trim api 25)
+			      (pylookup-trim module 30)
+			      (pylookup-trim type 10))))
+
+            (put-text-property
+             (line-beginning-position) (line-end-position)
+             'pylookup-target-url (cadr x))
+            (insert "\n"))
+
+	 ;; goto first entry
+         (goto-line 3)
+
+	 ;; turn mode on
+         (pylookup-mode)
+
+	 ;; highlighting
+         (font-lock-add-keywords nil `((,(format "\\(%s\\|%s\\|%s\\)" 
+						 search-term 
+						 (upcase search-term)
+						 (upcase-initials search-term))
+                                         1
+                                         font-lock-keyword-face prepend)))
+
+         (font-lock-add-keywords nil '(("\\<\\(lib\\)"
+                                         1
+                                         font-lock-constant-face prepend)))
+
+         (font-lock-add-keywords nil '(("\\<\\(in module.*)\\)"
+                                         1
+                                         font-lock-doc-face prepend)))
+
+	 ;; store window configuration
+         (set (make-local-variable 'pylookup-return-window-config) cur-window-conf)
+
+         ;; make fit to screen
+         (shrink-window-if-larger-than-buffer (get-buffer-window tmpbuf)))))))
+
+;;;###autoload
+(defun pylookup-update (src)
+  "Run pylookup-update and create the database at `pylookup-db-file'."
+  (interactive (list (ido-completing-read "Python Html Documentation source: "
+                                          pylookup-html-locations)))
+  
+  ;; pylookup.py -d /home/myuser/.pylookup/pylookup.db -l <URL>
+  (call-process pylookup-program nil standard-output nil 
+                       "-d" (expand-file-name pylookup-db-file) 
+		       "-l" src))
+
+(provide 'pylookup)
+;;; pylookup.el ends here
---
+++ pylookup.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+"""
+Pylookup is to lookup entries from python documentation, especially within emacs.
+Pylookup adopts most of ideas from haddoc, lovely toolkit by Martin Blais.
+"""
+
+debug = False
+
+import os, re, anydbm, urllib, htmllib, formatter
+from os.path import join, dirname, exists
+
+class IndexProcessor( htmllib.HTMLParser ):
+    """
+    Extract the index links from a Python HTML documentation index.
+    """
+    
+    def __init__( self, db, dirn ):
+        htmllib.HTMLParser.__init__( self, formatter.NullFormatter() )
+        
+        self.db         = db
+        self.dirn       = dirn
+        self.entry      = ""
+        self.desc       = ""
+        self.do_entry   = False
+        self.one_entry  = False
+        self.num_of_a   = 0
+
+    def start_dd( self, att ):
+        self.list_entry = True
+
+    def end_dd( self ):
+        self.list_entry = False
+
+    def start_dt( self, att ):
+        self.one_entry = True
+        self.num_of_a  = 0
+
+    def end_dt( self ):
+        self.do_entry = False
+        
+    def start_a( self, att ):
+        if self.one_entry:
+            self.url = join( self.dirn, dict( att )[ 'href' ] )
+            self.save_bgn()
+
+    def end_a( self ):
+        if self.one_entry:
+            if self.num_of_a == 0 :
+                self.desc = self.save_end()
+
+                # extract fist element
+                #  ex) __and__() (in module operator)
+                if not self.list_entry :
+                    self.entry = re.sub( "\([^)]+\)", "", self.desc )
+                
+                    match = re.search( "\([^)]+\)", self.desc )
+                    
+                    if match :
+                        self.desc = match.group(0)
+                        
+                self.desc = re.sub( "[()]", "", self.desc )
+
+            self.num_of_a += 1
+
+            # book : reference[ref]/c-api[api]/library[lib]/other[etc]
+            if self.url.find( "library" ) >= 0 :
+                book = "lib"
+            elif self.url.find( "c-api" ) >= 0 :
+                book = "api"
+            elif self.url.find( "reference" ) >= 0 :
+                book = "ref"
+            else :
+                book = "etc"
+
+            book = "[%s%s]" % ( book, "" if self.num_of_a == 1 else "/%d" % self.num_of_a )
+            key = "%s\t(%s)\t%s" % ( self.entry , self.desc, book )
+
+            if debug :
+                print "%s : %s" % ( key, self.url )
+
+            # store to db
+            self.db[ key ] = self.url
+        
+if __name__ == "__main__":
+
+    import optparse
+    
+    parser = optparse.OptionParser( __doc__.strip() )
+    
+    parser.add_option( "-d", "--db", dest="db", default="pylookup.db" )
+    parser.add_option( "-l", "--lookup", dest="key" )
+    parser.add_option( "-u", "--update", dest="url" )
+    parser.add_option( "-c", "--cache", action="store_true", default=False, dest="cache")
+
+    ( opts, args ) = parser.parse_args()
+
+    # create db
+    try:
+        db = anydbm.open( opts.db, 'c' )
+        
+    except anydbm.error :
+        raise SystemExit( "Error: Cannot access DB files" )
+
+    # update
+    if opts.url :
+
+        # trim
+        opts.url = opts.url if opts.url[-1] == "/" else opts.url + "/"
+
+        print "Wait for a few second (Fetching htmls from '%s'" % sopts.url
+
+        try:
+            index = urllib.urlopen( opts.url + "genindex-all.html" ).read()
+        except IOError, e:
+            raise SystemExit( "Error: fetching file from the web: '%s'" % e )
+        
+        parser = IndexProcessor( db, dirname( opts.url ) )
+        
+        parser.feed( index )
+        parser.close()
+
+    # cache
+    if opts.cache :
+        
+        cache = []
+        for key in db.keys() :
+            key = re.sub( "\([^)]*\)", "", key )
+            key = re.sub( "\[[^]]*\]", "", key )
+            cache.append( key.strip() )
+
+        # make it as unique
+        print "\n".join( list( set( cache ) ) )
+
+    # lookup
+    if opts.key :
+
+        keys = db.keys()
+        
+        results = []
+        
+        for term in opts.key.split() :
+            results.extend( ( x, db[x] ) for x in
+                            filter( re.compile( re.escape( term ), re.I ).search,
+                                    keys) )
+        
+        results.sort()
+
+        for key, url in results :
+            print '%s;%s' % ( key, url )
+
+    db.close()
