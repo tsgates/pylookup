tsgates/pylookup

commit f8e2a5b00939576f675ee5f990c4d4c7f51fd785

Removed unnecessary format check.
optparse already checks validity.

pylookup.py
@@ -223,6 +223,4 @@ if __name__ == "__main__":
         cache(opts.db)
 
     if opts.key:
-        format = TERMINAL_FORMAT if opts.format.lower() == "terminal"\
-                                  else EMACS_FORMAT
-        lookup(opts.db, opts.key, format=format)
+        lookup(opts.db, opts.key, format=opts.format)

commit a6068f1d17970113718dec97390d755b56f46425

Fixed regression from f8e2a5b00939576f675ee5f990c4d4c7f51fd785
It was not unnecessary after all. :(

pylookup.py
@@ -22,8 +22,10 @@ import formatter
 from os.path import join, dirname, exists, abspath, expanduser
 from contextlib import closing
 
-EMACS_FORMAT = "{entry}\t({desc})\t[{book}];{url}"
-TERMINAL_FORMAT = "{entry}\t({desc})\t[{book}]\n{url}s"
+FORMATS = {
+             "Emacs" : "{entry}\t({desc})\t[{book}];{url}",
+             "Terminal" : "{entry}\t({desc})\t[{book}]\n{url}"
+           }
 
 def build_book(s, num):
     """
@@ -221,4 +223,4 @@ if __name__ == "__main__":
         cache(opts.db)
 
     if opts.key:
-        lookup(opts.db, opts.key, opts.format)
+        lookup(opts.db, opts.key, FORMATS[opts.format])