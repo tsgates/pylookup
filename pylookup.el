6659af96b2ba47386de1e28c7672378981c83c94
Taesoo Kim <taesoo@mit.edu>
2012-12-03 15:43:43 -0500

    add emacs mirror head

--- pylookup.el
+++ pylookup.el
@@ -1,3 +1,29 @@
+;;; pylookup.el --- Look up python documents (reference) in Emacs
+
+;; Copyright (C) 2010-2013 Taesoo Kim
+
+;; Author: Taesoo Kim <taesoo@mit.edu>
+;; Maintainer: Taesoo Kim <taesoo@mit.edu>
+;; Created: 19 June 2009
+;; Keywords: python,reference,document,help
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; 
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (eval-when-compile
   (require 'browse-url)
   (require 'simple)
